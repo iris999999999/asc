c5dd9e4097684d73d933087e1fb0db4d93f37f70
irina <iris_9@inbox.ru>
2020-04-17 16:38:21 +0300

    pdf added

--- device_message/views.py
+++ device_message/views.py
@@ -39,9 +39,8 @@ def get_sql(form_organization,date_time):
     conn = psycopg2.connect(conn_string) 
     #frame_query принимает результат sql запроса, а вовзращает DataFrame 
     df  = pd.read_sql(sql, conn)
-    df.to_csv('123.csv')   
+    #df.to_csv('123.csv')   
 
-   
     date_1 = date_time["datetime1"].date()  
     date_2 = date_time["datetime2"].date()
 
@@ -51,15 +50,6 @@ def get_sql(form_organization,date_time):
    
     df_ = df.groupby(['oo_name','oo_id','dd_name','pp_sur_name','pp_name','pp_patronymic','jj_name','date_f','time_f','readerID']).size().reset_index(name='count').query('(date_f >= @date_1)&(date_f <= @date_2)&(time_f >= @time_1)&(time_f <= @time_2)').copy()
    
-    print(999999999999999999999999999999999999)
-    print(df_)
-    print(55555555555555555555555555555555555555)
-    print(time_1)
-    print(55555555555555555555555555555555555555)
-    print(999999999999999999999999999999999999)
-
-
-      
     df1 = df_[['oo_name','dd_name','oo_id']].copy()
     id_organization = 1
     if int(form_organization) == 2:
@@ -78,11 +68,7 @@ def get_sql(form_organization,date_time):
     
     df1 = df_[['oo_name','dd_name','pp_sur_name','pp_name','pp_patronymic','jj_name','date_f','time_f','readerID']].copy()
     devices_messages = df1.groupby(['oo_name','dd_name','pp_sur_name','pp_name','pp_patronymic','jj_name','date_f','time_f','readerID']).size().reset_index(name='count') 
-    print(777777777777777)
-    print(devices_messages)
-
-
-
+   
     now = datetime.now()
         
     return { "organizations_departaments":organizations_departaments,
@@ -103,17 +89,7 @@ def messages(request):
     
     return render(request,'device_message.html',get_sql(1,dateTimeF()))
 
-def table_pdf(data,pdf,spacing=1):   
- 
-    col_width = pdf.w / 4.5
-    row_height = pdf.font_size
-    for row in data:
-        for item in row:
-            pdf.cell(col_width, row_height*spacing,
-                     txt=item, border=1)
-        pdf.ln(row_height)
 
-    return pdf
 
 def refreshPage(request):   
     form = DateForm(request.GET)
@@ -136,7 +112,7 @@ def PageBootstrap(request):
         #import pdb; pdb.set_trace()
         if form.is_valid():
           form_organization_= form.cleaned_data.get("organization_list") 
-        print(formD.is_valid())  
+         
         if formD.is_valid():
           
           date1_= formD.cleaned_data.get("date1") 
@@ -148,6 +124,25 @@ def PageBootstrap(request):
     else:
         return HttpResponseNotAllowed()
 
+def table_pdf(data,pdf,spacing=1):   
+ 
+    #col_width = pdf.w / 4.5
+    #row_height = pdf.font_size
+
+    col_width = 40
+    row_height = 7
+
+    for row in data:
+        pdf.cell(40)
+        pdf.set_fill_color(180,0,157)
+        for item in row:
+            
+            pdf.cell(col_width, row_height*spacing,
+                     txt=item, align="C", border=1)
+            
+        pdf.ln(row_height)
+
+    return pdf
 
 def save_from_html(request): 
     
@@ -160,38 +155,52 @@ def save_from_html(request):
 
     pdf = FPDF()
     pdf.add_page()
-    font = '/home/irina/Documents/python_projects/asc/font/DejaVuSansCondensed.ttf'
+    font = '/home/irina/Documents/python_projects/asc/font/DejaVuSansCondensed-Bold.ttf'
+    fontB = '/home/irina/Documents/python_projects/asc/font/DejaVuSansCondensed.ttf'
     pdf.add_font('DejaVu', '', font, uni=True)
-    pdf.set_font('DejaVu', '', 14)
-    pdf.cell(200, 10, txt="Отчет по проходам", ln=1, align="C")
+    pdf.set_font('DejaVu', '', 7)
+    pdf.cell(190, 7, txt="Система контроля и управления доступом ASC32", ln=1, align="R")
     
-    for sub_heading in soup.find_all('h3'):
-        pdf.add_font('DejaVu', '', font, uni=True)
-        pdf.set_font('DejaVu', '', 13)          
-        pdf.cell(0, 10, txt="{}".format(sub_heading.text.strip()), ln=1)
+    #pdf.add_font('DejaVu', '', font, uni=True)
+    pdf.set_font('DejaVu', '', 7)
+    pdf.cell(190, 7, txt="{}".format(soup.find('div',id='date_time_now').text.strip()), ln=1, align="R")
+    
+    #pdf.add_font('DejaVu', '', fontB, uni=True)
+    pdf.set_font('DejaVu', '', 14)
+    pdf.cell(200, 10, txt="Отчет по контролю времени входа/выхода персонала на предприятии.", ln=1, align="C")
+    
+    for sub_heading in soup.find_all('div',id='organizations_departaments'):
+        #pdf.add_font('DejaVu', 'B', fontB, uni=True)
+        pdf.set_font('DejaVu', '', 13)  
+        pdf.set_text_color(222,154,255)         
+        pdf.cell(0, 10, txt="{}".format(sub_heading.text.strip()), ln=1, align="C")
         
-        for sub_heading1 in soup.find_all('h4'):
-            if sub_heading1.find_previous('h3').text == sub_heading.text:
+        for sub_heading1 in soup.find_all('div',id='persons_jobs'):
+            if sub_heading1.find_previous('div',id='organizations_departaments').text == sub_heading.text:
             
-                pdf.add_font('DejaVu', '', font, uni=True)
+                #pdf.add_font('DejaVu', '', font, uni=True)
                 pdf.set_font('DejaVu', '', 11)
-                pdf.cell(0, 10, txt="{}".format(sub_heading1.text.strip()), ln=1)
+                pdf.set_text_color(0,0,0)  
+                pdf.cell(0, 10, txt="{}".format(sub_heading1.text.strip()), ln=1, align="C")
                 
                 data = []
                 data1 = []
-                data.append(['Дата','Проход','Считыватель'])
+                data.append(['Дата','Время','Считыватель'])
                 k = 1
                 
                 for sub_heading2 in soup.find_all('tr'):
                     
-                    if sub_heading2.find_previous('h4') != None:
-                        if sub_heading2.find_previous('h4').text == sub_heading1.text:
+                    if sub_heading2.find_previous('div',id='persons_jobs') != None:
+                        if sub_heading2.find_previous('div',id='persons_jobs').text == sub_heading1.text:
                             if  (sub_heading2.text.strip() !=""):
                                 if (sub_heading2.text.find("Дата") == -1) and (sub_heading2.text.find("Проход") == -1) and (sub_heading2.text.find("Считыватель") == -1):
                                     data.append(sub_heading2.text.strip().split('\n'))
                                             
 
                 
+                pdf.set_font('DejaVu', '', 9)
+                pdf.set_text_color(0,0,0)
+
                 pdf = table_pdf(data,pdf) 
 
     pdf.output("device_message.pdf")
